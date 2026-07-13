4746deb1e92dce66a313778d53c31df2eda2bc98
Trong <trongnb@flyer.us>
2026-07-13 16:49:56 +0700

    style(nix): clean up trailing whitespace in home/macos.nix

--- home/macos.nix
+++ home/macos.nix
@@ -3,7 +3,7 @@
 
   home.username = user;
   home.homeDirectory = "/Users/${user}";
-  
+
   # Keep this value. It ensures backward compatibility with the version you started with.
   home.stateVersion = "24.11";
 }
