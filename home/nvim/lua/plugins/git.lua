d316fd8bb90957852e21ff1db7834b761dce3189
Trong <trongnb@flyer.us>
2026-07-15 14:43:22 +0700

    feat(nvim): configure shortcuts for git diff view and file history

--- home/nvim/lua/plugins/git.lua
+++ home/nvim/lua/plugins/git.lua
@@ -1,12 +1,26 @@
 return {
   {
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<CR>', desc = 'Git Diff View' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory %<CR>', desc = 'File Git History' },
+      { '<leader>gq', '<cmd>DiffviewClose<CR>', desc = 'Close Git Diff' },
+    },
+  },
+  {
     'NeogitOrg/neogit',
     dependencies = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' },
-    keys = { { '<leader>g', function() require('neogit').open() end, desc = 'Neogit' } },
+    keys = {
+      { '<leader>g', function() require('neogit').open() end, desc = 'Neogit' },
+    },
   },
   {
     'lewis6991/gitsigns.nvim',
     event = 'BufWinEnter',
     opts = { current_line_blame = true },  -- who last touched this line
+    keys = {
+      { '<leader>gs', '<cmd>Gitsigns diffthis<CR>', desc = 'Git Diff current file' },
+    },
   },
 }
